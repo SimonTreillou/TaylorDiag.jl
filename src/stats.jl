e46730282ddce93bed29e8834207ea1becb6ff7f
SimonTreillou <simontreillou@orange.fr>
2022-10-01 19:51:54 +0200

    add stats and tests

---
+++ src/stats.jl
@@ -0,0 +1,43 @@
+"""
+    mean(C::AbstractArray)
+
+Compute the mean of the collection C.
+"""
+function mean(C::AbstractArray)
+    sum(C)/length(C)
+end
+
+"""
+    STD(C::AbstractArray)
+
+Compute the standard deviation of the collection C.
+"""
+function STD(C::AbstractArray)
+    sqrt.( sum( (C.-mean(C)).^2 ) ./ length(C) )
+end
+
+"""
+    RMSD(Cr::AbstractArray,C::AbstractArray)
+
+Compute the root mean-squared deviation between reference collection Cr and C.
+"""
+function RMSD(Cr::AbstractArray,C::AbstractArray)
+    sqrt.( sum( ( C.-mean(C) .- Cr .+ mean(Cr)).^2 ) ./ length(C))
+end
+
+"""
+    COR(Cr::AbstractArray,C::AbstractArray)
+
+Compute the correlation coefficient between reference collection Cr and C.
+Note: the function exactly returns the minimum between 1.0 and the formula because of numerical errors.
+```julia
+Cr = [1.0, 2.0, 3.0]
+C  = [1.0, 2.0, 3.0]
+Correlation = sum( (C.-mean(C)) .* (Cr.-mean(Cr)) ) ./ (length(C) .* STD(C) .* STD(Cr))
+```
+In this case, `correlation=1.0000000000000002`, or `correlation=1+2e-16`, being the computational error because of (1/3) (and multiples) rounding.
+
+"""
+function COR(Cr,C)
+    round(sum( (C.-mean(C)) .* (Cr.-mean(Cr)) ) ./ (length(C) .* STD(C) .* STD(Cr)),digits=15)
+end
---
+++ test/test-statistics.jl
@@ -0,0 +1,40 @@
+@testset "Standard deviation" begin
+    C1 = [1.0, 1.0, 1.0]
+    C2 = [1.0, 0.0, 0.0, 1.0]
+    @test STD(C1) == 0.
+    @test STD(C2) == 0.5
+end;
+
+@testset "Mean" begin
+    C1 = [1.0, 1.0, 1.0]
+    C2 = [1.0, 0.0, 0.0, 1.0]
+    @test mean(C1) == 1.0
+    @test mean(C2) == 0.5
+end;
+
+@testset "RMSD" begin
+    Cr = [1.0, 1.0, 1.0]
+    C1 = [1.0, 1.0, 1.0]
+    C2 = [0.0, 0.0, 0.0]
+    C3 = [1.0, 2.0, 3.0]
+    @test RMSD(Cr,C1) == 0.
+    @test RMSD(Cr,C2) == 0.
+    @test RMSD(Cr,C2) == 0.816496580927726
+end;
+
+@testset "Correlation coefficient" begin
+    Cr = [1.0, 2.0, 3.0]
+    C1 = [1.0, 1.0, 1.0]
+    C2 = [0.0, 0.0, 0.0]
+    C3 = [1.0, 2.0, 3.0]
+    CrRand = rand(10) 
+    C1Rand = CrRand * 104.4
+    C2Rand = CrRand * 104.4 .+ 154.
+    C3Rand = CrRand * 104.4 .+ 154.0*rand(10)
+    @test isnan(COR(Cr,C1))
+    @test isnan(COR(Cr,C2))
+    @test COR(Cr,C3) == 1.0
+    @test COR(CrRand,C1Rand) == 1.0
+    @test COR(CrRand,C2Rand) == 1.0
+    @test COR(CrRand,C3Rand) != 1.0
+end;
