e5d4103f31a0e45586064c99055f51cbb47c6331
SimonTreillou <simontreillou@orange.fr>
2022-10-02 13:32:11 +0200

    fix test plotting

--- test/test-plotting.jl
+++ test/test-plotting.jl
@@ -1,7 +1,7 @@
 @testset "Relation between STD-COR-RMSD" begin
     Cr = rand(10)*10 
     C  = Cr + rand(10)
-    @test isapprox(taylor_verification(Cr,C), 0.0; atol=1e-14, rtol=0)
+    @test isapprox(taylor_verification(Cr,C), 0.0; atol=1e-13, rtol=0)
 end;
 
 
